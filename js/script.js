2a9094676e44f1449659d5979fedacb068c827cb
Lubin Mendez <mendezlubin@yahoo.com>
2026-05-17 17:39:34 -0700

    fix: close mobile menu when mobile navigation link is clicked

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const hamburguerButton = document.querySelector(".menu-hamburguer-button");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuLinks = document.querySelectorAll(".mobile-menu a");
 
+/* Toggles mobile menu visibility when hamburger button is clicked */
 hamburguerButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
+});
+
+/* Closes mobile menu when a navigation link is clicked */
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+  });
 });
