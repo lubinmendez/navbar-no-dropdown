2eabe1fed08ac203399ba1a97d80565f9c8be127
Lubin Mendez <mendezlubin@yahoo.com>
2026-05-23 21:24:00 -0700

    Close mobile menu when clicking the logo-name

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 const hamburguerButton = document.querySelector(".menu-hamburguer-button");
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuLinks = document.querySelectorAll(".mobile-menu a");
+const logoName =document.querySelector(".logo-name-link");
 
-/* Toggles mobile menu visibility when hamburger button is clicked */
+// Toggles mobile menu visibility when hamburger button is clicked
 hamburguerButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
 });
 
-/* Closes mobile menu when a navigation link is clicked */
+// Closes mobile menu when a navigation link is clicked
 mobileMenuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileMenu.classList.remove("show");
   });
+});
+
+// Close the mobile menu when the logo-name is clicked so the home section becomes visible
+logoName.addEventListener("click", () => {
+  mobileMenu.classList.remove("show");
 });
