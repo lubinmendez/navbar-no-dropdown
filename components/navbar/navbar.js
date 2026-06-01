44748e5b0d21e03681d38943d13bca3618e80f7d
Lubin Mendez <mendezlubin@yahoo.com>
2026-05-31 17:02:49 -0700

    Update aria-expanded state with JavaScript for mobile menu accessibility

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,21 +1,30 @@
 const hamburgerButton = document.querySelector(".menu-hamburger-button");
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuLinks = document.querySelectorAll(".mobile-menu a");
-const logoName =document.querySelector(".logo-name-link");
+const logoName = document.querySelector(".logo-name-link");
 
-// Toggles mobile menu visibility when hamburger button is clicked
-hamburgerButton.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show");
-});
+// =========== Functions ==============
+
+// I am also updating aria-expanded to reflect the current mobile menu state for accessibility (screen readers)
+
+function closeMobileMenu() {
+  mobileMenu.classList.remove("show");
+  hamburgerButton.setAttribute("aria-expanded", false);
+}
+
+function toggleMobileMenu() {
+  const isOpen = mobileMenu.classList.toggle("show"); // Toggles mobile menu visibility and returns whether it is open (true) or closed (false)
+  hamburgerButton.setAttribute("aria-expanded", isOpen);
+}
+// ============== Event listeners================= //
 
 // Closes mobile menu when a navigation link is clicked
 mobileMenuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    mobileMenu.classList.remove("show");
-  });
+  link.addEventListener("click", closeMobileMenu);
 });
 
 // Close the mobile menu when the logo-name is clicked so the home section becomes visible
-logoName.addEventListener("click", () => {
-  mobileMenu.classList.remove("show");
-});
+logoName.addEventListener("click", closeMobileMenu);
+
+// Toggles mobile menu visibility when hamburger button is clicked
+hamburgerButton.addEventListener("click", toggleMobileMenu);
